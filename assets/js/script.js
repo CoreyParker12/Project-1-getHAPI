572918046034bba9b71cf849ef2707f78e8a2b37
osiyo <merewall@gmail.com>
2021-06-02 19:21:06 -0400

    adding sidebar html, css styling, and local storage JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+// Empty array to push generated APIs into...
+let surpriseArray = [];
+
 // Random Facts fetch and rendering to page
 
 //Somewhere, after click
@@ -28,7 +31,9 @@ function getRandomFact() {
       return response.json();
     })
     .then(function (data) {
-        $selectedAPI.text(data.text)
+        $selectedAPI.text(data.text);
+        surpriseArray.push(data.text);
+        storeSurprise();
     });
 
 }
@@ -49,6 +54,8 @@ function getDadJoke() {
   })
   .then(function (data) {
     $selectedAPI.text(data.joke);
+    surpriseArray.push(data.joke);
+    storeSurprise();
   });
 
 }
@@ -65,6 +72,8 @@ fetch('https://type.fit/api/quotes')
   .then(function (data) {
         let randomIndex = Math.floor(Math.random()*data.length)
         $selectedAPI.text('"' + data[randomIndex].text + '"' + "  -" + data[randomIndex].author);
+        surpriseArray.push('"' + data[randomIndex].text + '"' + "  -" + data[randomIndex].author);
+        storeSurprise();
   });
 
 }
@@ -83,6 +92,53 @@ $('#feelingBtn').click(function() {
   });
 
 
+// Click event listener for "Smart" button choice under "I'm Feeling"
+$('#factBtn').click(function() {
+  $("#homepage").hide();
+  $("#renderedAPI").show();
+  getRandomFact();
+});
 
+// Click event listener for "Punny" button choice under "I'm Feeling"
+$('#dadBtn').click(function() {
+  $("#homepage").hide();
+  $("#renderedAPI").show();
+  getDadJoke();
+});
 
+// Click event listener for "Inspired" button choice under "I'm Feeling"
+$('#quoteBtn').click(function() {
+  $("#homepage").hide();
+  $("#renderedAPI").show();
+  getQuote();
+});
 
+// to copy over to JS...
+// empty array variable
+// 2 lines of code in each API function to 1. push surprise, 2. store surprise
+// storeSurprise function
+// render Surprise function
+
+// Creating a function to store an array of generated 'surprises' to local storage
+function storeSurprise() {
+  localStorage.setItem("surprise", JSON.stringify(surpriseArray));
+}
+
+// Creating a function to render the saved 'surprises' to the page from local storage
+function renderSurprises() {
+  let storedSurprises = JSON.parse(localStorage.getItem("surprise"));
+
+  // If there are surprises saved to local storage, set the array of surprises to those
+  if(storedSurprises) {
+      surpriseArray = storedSurprises;
+      
+      // For each stored surprise in local storage, render it to the page
+      // for (let i=0; i < storedSurprises.length; i++) {
+      //     // change the jQuery selector based on Corey's div name
+      //     let previousSurprise = $('<p>').text(surpriseArray[i]);
+      //     NAME_OF_DIV_TO_PUT_SURPRISES_IN.append(previousSurprise);
+      // }
+  }
+}
+// Show any saved surprises upon page load
+renderSurprises();
