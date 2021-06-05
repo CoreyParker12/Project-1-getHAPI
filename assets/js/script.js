366b50b48b36708d9870aef9b58a6b7de8861dc5
osiyo <merewall@gmail.com>
2021-06-05 10:51:42 -0400

    adding sort function and rating stars to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -126,9 +126,10 @@ $('#form-styling').submit(function(event) {
   event.preventDefault();
 
 
-  let userRating = event.currentTarget[0].value;
+  let userRating = parseInt(event.currentTarget[0].value);
+  console.log(typeof(userRating));
 
-  if (userRating == 1 || userRating == 2 || userRating == 3 || userRating == 4 || userRating == 5) {
+  if (userRating === 1 || userRating === 2 || userRating === 3 || userRating === 4 || userRating === 5) {
     let latestAPI = $('#selected-API').text();
 
     let userObject = {
@@ -146,8 +147,9 @@ $('#form-styling').submit(function(event) {
     alert('No!');
   }
 
+  $('#user-rating').val("");
 
-
+  location.reload();
 });
 
 
@@ -158,11 +160,57 @@ function renderSurprises() {
   // If there are surprises saved to local storage, set the array of surprises to those
   if(storedSurprises) {
       surpriseArray = storedSurprises;
+
+      // SORTING ARRAY FUNCTION
+      // https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
+      function compare(a, b) {
+        let ratingA = a.rating;
+        let ratingB = b.rating;
+
+        let comparison = 0;
+        if (ratingA > ratingB) {
+          comparison = -1;
+        } else if (ratingA < ratingB) {
+          comparison = 1;
+        }
+        return comparison
+      }
+      
+      surpriseArray.sort(compare);
+      console.log(surpriseArray);
+      // -------------------------------------------------------------------
       
       // For each stored surprise in local storage, render it to the page
+      // adding in if statement to change rating into stars
+      let numOfStars = "";
       for (let i=0; i < 5; i++) {
-          // change the jQuery selector based on Corey's div name
-          let previousSurprise = $('<button>').html(surpriseArray[i].API + '<br></br>' + surpriseArray[i].rating);
+          if (surpriseArray[i].rating === 1) {
+            numOfStars = "☆";
+          } else if (surpriseArray[i].rating === 2) {
+            numOfStars = "☆☆";
+          } else if (surpriseArray[i].rating === 3) {
+            numOfStars = "☆☆☆";
+          }  else if (surpriseArray[i].rating === 4) {
+            numOfStars = "☆☆☆☆";
+          }  else {numOfStars = "☆☆☆☆☆";
+          
+          }
+
+      //     let numOfStars = "";
+      // for (let i=0; i < 5; i++) {
+      //     if (surpriseArray[i].rating === 1 || surpriseArray[i].rating === "☆") {
+      //       let numOfStars = "☆";
+      //     } else if (surpriseArray[i].rating === 2 || surpriseArray[i].rating === "☆☆") {
+      //       let numOfStars = "☆☆";
+      //     } else if (surpriseArray[i].rating === 3 || surpriseArray[i].rating === "☆☆☆") {
+      //       let numOfStars = "☆☆☆";
+      //     }  else if (surpriseArray[i].rating === 4 || surpriseArray[i].rating === "☆☆☆☆") {
+      //       let numOfStars = "☆☆☆☆";
+      //     }  else {let numOfStars = "☆☆☆☆☆";
+          
+      //     }
+        // ------------------------------------------------------
+          let previousSurprise = $('<button>').html(surpriseArray[i].API + '<br></br>' + numOfStars);
           $('#recent-results').append(previousSurprise);
 
           if (!surpriseArray[i+1]) {
@@ -178,10 +226,15 @@ function renderSurprises() {
 renderSurprises();
 
 // Click event listener for return to homepage button
-$('#returnBtn').click(function() {
-  // $("#renderedAPI").hide();
-  // $("#homepage").show();
-  // $('#selected-API').empty();
-  // $("#myDropdown").hide();
-  location.reload();
-})
+// $('#returnBtn').click(function() {
+//   // $("#renderedAPI").hide();
+//   // $("#homepage").show();
+//   // $('#selected-API').empty();
+//   // $("#myDropdown").hide();
+//   location.reload();
+// })
+
+// line 149 - reset form input to blank
+// should we remove Return to Homepage button and just capture that with the submit button?
+// LINES 163 to 180 for sorting function
+// lines 183 to 195 for if statement changing ratings into stars
